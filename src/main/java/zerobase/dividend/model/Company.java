fa997f19c33d75964c282e50a8a093f5afed7ce0
yjson <handyejee14@gmail.com>
2024-07-08 15:00:53 +0900

    회사정보, 배당금 정보 엔티티 부분

---
+++ src/main/java/zerobase/dividend/model/Company.java
@@ -0,0 +1,12 @@
+package zerobase.dividend.model;
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Company {
+
+    private String ticker;
+    private String name;
+
+}
